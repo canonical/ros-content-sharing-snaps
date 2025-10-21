947bf1203ed9f47bb31cfea434bd3b7461c38cb4
Giuseppe Barbieri <giuseppe.barbieri@canonical.com>
2025-10-21 15:09:30 +0200

    remove debugging and build all snaps

--- generate_all_ros_esm_meta_snapcraft_file.py
+++ generate_all_ros_esm_meta_snapcraft_file.py
@@ -44,8 +44,7 @@ def main(args=None):
     ros_distros_content_sharing_snaps = [
         ROSContentSharingSnapVariants(
             ros_distro = "noetic",
-            # variants = ["ros-core", "ros-base", "robot", "desktop"],
-            variants = ["ros-core"],
+            variants = ["ros-core", "ros-base", "robot", "desktop"],
             architectures = ["amd64", "arm64"]),
         ]
 
